2ac8295f87140432ccd0eb11859bea71b4c27719
moehnked <moehnked@oregonstate.edu>
2019-10-18 17:23:16 -0700

    initial commit

---
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,12 @@
+package com.example.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment(){
+        return new CrimeFragment();
+    }
+}
